ef1ecaeedb0656dd5eab0e3ea621aca097bcf972
MuhammadSyulhan <janmuno77@gmail.com>
2020-04-25 15:26:17 +0800

    pnambah intent baru

---
+++ learnThreeKotlin/app/src/main/java/com/example/learnthreekotlin/MenuAwal.kt
@@ -0,0 +1,34 @@
+package com.example.learnthreekotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MenuAwal : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_awal)
+
+        val btnBelanja: Button = this.findViewById(R.id.btn_belanja)
+        btnBelanja.setOnClickListener(this)
+
+        val btnCekhp: Button = this.findViewById(R.id.btn_cekhp)
+        btnCekhp.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v.id  == R.id.btn_belanja) {
+            val Intent = Intent(this@MenuAwal, MoveActivityOne::class.java)
+            startActivity(Intent)
+        }
+
+        else if (v.id  == R.id.btn_cekhp) {
+            val Intent = Intent(this@MenuAwal, MoveActivityOne1::class.java)
+            startActivity(Intent)
+        }
+    }
+}
