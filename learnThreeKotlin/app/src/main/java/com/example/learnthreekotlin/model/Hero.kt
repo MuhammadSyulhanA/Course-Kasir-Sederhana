57fae2d8660aae1af0950e9b6b30c147a5eb16ee
MuhammadSyulhan <janmuno77@gmail.com>
2020-04-26 15:12:31 +0800

    tambah folder, class, foto

---
+++ learnThreeKotlin/app/src/main/java/com/example/learnthreekotlin/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.example.learnthreekotlin.model
+
+data class Hero (
+    //kalo kotlin gaperlu lagi dibikinin constructor, setter, getter
+
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+
+)
---
+++ learnThreeKotlin/app/src/main/java/com/example/learnthreekotlin/model/HeroesData.kt
@@ -0,0 +1,5 @@
+package com.example.learnthreekotlin.model
+
+class HeroesData(
+
+)
