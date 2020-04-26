fad276de24847872d2f286d8680bce94108a976c
MuhammadSyulhan <janmuno77@gmail.com>
2020-04-26 14:33:43 +0800

    perbaiki style sama tambah activity2

--- learnThreeKotlin/app/src/main/java/com/example/learnthreekotlin/MoveActivityOne2.kt
+++ learnThreeKotlin/app/src/main/java/com/example/learnthreekotlin/MoveActivityOne2.kt
@@ -3,10 +3,10 @@ package com.example.learnthreekotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MoveActvityOne2 : AppCompatActivity() {
+class MoveActivityOne2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_move_actvity_one2)
+        setContentView(R.layout.activity_move_one2)
     }
 }
